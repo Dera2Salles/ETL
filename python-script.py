ed2682c304c39ca2a7c6df482dc07761b7ee348d
Dera2S <dera.ah.14@gmail.com>
2025-11-20 13:00:52 +0300

    fix : fix product code index

--- python-script.py
+++ python-script.py
@@ -6,7 +6,7 @@ dataFrameBranch = pandas.read_excel('./dataset_account.xlsx' , sheet_name='branc
 
 dataFrame['BankCode'] = dataFrame['noCompte'].str[0:5]
 dataFrame['BranchCode'] = dataFrame['noCompte'].str[5:10]
-dataFrame['ProductCode'] = dataFrame['noCompte'].str[10]
+dataFrame['ProductCode'] = dataFrame['noCompte'].str[12]
 dataFrame['AccountNumber'] = dataFrame['noCompte'].str[11:21]
 
 dataFrameMerged = dataFrame.merge(dataFrameProduct, on='ProductCode', how='left')
